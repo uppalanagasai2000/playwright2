1d7ead93e2a4b9c6ee745a58f36388e2bf17519e
WorkspaceBot <uppalanagasai2000@gmail.com>
2026-02-22 11:41:34 +0530

    all files added

---
+++ Practice_02/test_01.js
@@ -0,0 +1,20 @@
+let marsksScored=10;
+if(marsksScored>=90 || marsksScored<=100){
+    console.log("Grade A");
+} else if(marsksScored>=80 && marsksScored<=89){
+    console.log("Grade B");
+}
+else if(marsksScored>=70 && marsksScored<=79){
+    console.log("Grade C");
+}
+else if(marsksScored>=60 && marsksScored<=69){
+    console.log("Grade D");
+}
+else if(marsksScored>=0 && marsksScored<=59){
+    console.log("Grade F");
+}
+else{
+    console.log("Invalid marks");
+}   
+
+    
